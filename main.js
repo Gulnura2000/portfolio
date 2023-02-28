d15ade4f715ed9f072444e86ec4e83bf080ec042
gulnura2000 <pisatovagulnura.2000@gmail.com>
2023-02-28 13:12:02 +0300

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+// function f(){
+//     $('.imgBlock').css({'width' : "100%" , "opacity": "1"})
+// }
+// f()
+
+
+// let burgerIcon = document.querySelector('#burgerIcon')
+// let list = document.querySelector('.list')
+// burgerIcon.onclick = ()=>{
+//     list.classList.toggle('newlist')
+// }
